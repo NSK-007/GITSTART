c7459218f09d7a29107f8d00a658a38eec13213c
NSK-007 <nshiva900@gmail.com>
2022-12-29 10:52:47 +0530

    adding new li

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ var items = document.querySelectorAll('li');
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight = 'bold';
 }
+
+var element = document.getElementsByTagName('ul');
+var node = document.createElement('li');
+var textNode = document.createTextNode('Item 5');
+node.appendChild(textNode);
+// node.classList.add('list-group-item');
+element[0].appendChild(node);
