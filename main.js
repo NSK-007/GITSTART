8adf0e9d5222c2edbf7e20ffc420fbb61f772a28
NSK-007 <nshiva900@gmail.com>
2022-12-29 12:21:29 +0530

    adding more DOM manipulations

--- main.js
+++ main.js
@@ -11,9 +11,35 @@ var node = document.createElement('li');
 var textNode = document.createTextNode('Item 5');
 node.appendChild(textNode);
 // node.classList.add('list-group-item');
-element[0].appendChild(node);
+// element[0].appendChild(node);
 
 var element_3 = document.getElementsByClassName('list-group-item');
 element_3 = element_3[2];
 
 element_3.style.display = 'none';
+
+var itemList = document.querySelector('#items');
+console.log(itemList.parentElement);
+console.log(itemList.lastElementChild);
+console.log(itemList.firstElementChild);
+console.log(itemList.nextElementSibling);
+console.log(itemList.previousElementSibling);
+
+var newDiv = document.createElement('div');
+var text = document.createTextNode('Hello World!');
+newDiv.appendChild(text);
+console.log(newDiv);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv, h1);
+
+var ul = document.querySelector('ul');
+var li = document.querySelector('.list-group-item');
+
+ul.insertBefore(newDiv, li);
+
+
+
+
